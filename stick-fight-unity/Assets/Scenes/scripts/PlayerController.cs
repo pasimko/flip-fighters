774ce0fe8ceb20d7341ff2eea1f5df76757d78a4
Paul <pasimko13@gmail.com>
2019-03-01 11:55:17 -0800

    Workin on upright force

--- stick-fight-unity/Assets/Scenes/scripts/PlayerController.cs
+++ stick-fight-unity/Assets/Scenes/scripts/PlayerController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody2D head;
+    public Transform leftLeg, rightLeg;
+    bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isGrounded)
+        {
+            head.AddForce(new Vector2(0, 40));
+            Debug.Log(1);
+        }
+        head.AddForce(new Vector2(0, 40));
+    }
+
+    void GroundCheck()
+    {
+        RaycastHit hit;
+        float distance = 1f;
+        Vector3 dir = new Vector3(0, -1);
+
+        if (Physics.Raycast(leftLeg.position, dir, out hit, distance))
+        {
+            isGrounded = true;
+        }
+        else if (Physics.Raycast(rightLeg.position, dir, out hit, distance))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
     }
 }
