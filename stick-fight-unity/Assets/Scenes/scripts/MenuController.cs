1d55119b82dab96987db3c9920fdb649584b9bc9
Ike <ike.lear@gmail.com>
2019-03-05 11:19:04 -0800

    Control and Setting Scene Creation

--- stick-fight-unity/Assets/Scenes/scripts/MenuController.cs
+++ stick-fight-unity/Assets/Scenes/scripts/MenuController.cs
@@ -3,11 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuController : MonoBehaviour {
+public class MenuController : MonoBehaviour
+{
 
-    public void startGame ()
+    bool controlsOn = false;
+    bool settingsOn = false;
+    bool startOn = false;
+
+    public void startGame()
     {
         SceneManager.LoadScene(sceneName: "mainScene");
     }
 
+    public void goControls()
+    {
+        SceneManager.LoadScene(sceneName: "controls");
+    }
+
+    public void backToMenu()
+    {
+        SceneManager.LoadScene(sceneName: "menu");
+    }
+
+    public void goSettings()
+    {
+        SceneManager.LoadScene(sceneName: "settings");
+    }
+
 }
+
+
