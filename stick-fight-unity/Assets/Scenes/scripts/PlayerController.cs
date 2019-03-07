5c3b4437d7781a115a2767e1abd2f4a00a41b5a4
Paul <pasimko13@gmail.com>
2019-03-07 10:38:04 -0800

    ada

--- stick-fight-unity/Assets/Scenes/scripts/PlayerController.cs
+++ stick-fight-unity/Assets/Scenes/scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D head;
+    public Rigidbody2D head, torso;
     public Transform leftLeg, rightLeg, rightToe, leftToe;
     bool isGrounded = false;
 
@@ -20,9 +20,17 @@ public class PlayerController : MonoBehaviour
     {
         isGrounded = (Physics2D.OverlapCircle(leftToe.position, 0.2f, groundLayer) || Physics2D.OverlapCircle(rightToe.position, 0.2f, groundLayer));
         Debug.Log(isGrounded);
-        if (isGrounded) {
-            head.AddForce(new Vector2(0, 40));
+        standUp();
+    }
+
+    void standUp()
+    {
+        if (isGrounded)
+        {
+            head.AddForce(new Vector2(0, 30));
+            torso.AddForce(new Vector2(0, 20));
         }
+
     }
    
 }
