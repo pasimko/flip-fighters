d703687e70ddba64c1f488bbdcde401095ff60e6
Anders <anderscpeterson@gmail.com>
2019-04-10 10:43:17 -0700

    Player Spawns!!

--- stick-fight-unity/Assets/Scenes/scripts/GameController.cs
+++ stick-fight-unity/Assets/Scenes/scripts/GameController.cs
@@ -3,23 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameController : MonoBehaviour {
-    public Transform stickPlayer;
-    public List<Transform> stickPlayerList = new List<Transform>();
+public class GameController : MonoBehaviour
+{
+    public Transform stickPlayer1;
+    public Transform stickPlayer2;
+
 
     void Awake()
     {
-        for (int i = 0; i < 2; i++)
-        {
-            GameObject tempPlayerObj = new GameObject();
-            Transform tempPlayerTransform = tempPlayerObj.transform;
-            Vector3 stickPlayerPos = new Vector3(Random.Range(-5, 5), 6, 10);
-            tempPlayerTransform = Instantiate(stickPlayer, stickPlayerPos, Quaternion.identity);
-            stickPlayerList.Add(tempPlayerTransform);
-        }
+
+        GameObject tempPlayer1Obj = new GameObject();
+        Transform tempPlayer1Transform = tempPlayer1Obj.transform;
+        Vector3 stickPlayer1Pos = new Vector3(Random.Range(-16, -3), 6, 10);
+        tempPlayer1Transform = Instantiate(stickPlayer1, stickPlayer1Pos, Quaternion.identity);
+
+        GameObject tempPlayer2Obj = new GameObject();
+        Transform tempPlayer2Transform = tempPlayer2Obj.transform;
+        Vector3 stickPlayer2Pos = new Vector3(Random.Range(0, 9), 6, 10);
+        tempPlayer2Transform = Instantiate(stickPlayer2, stickPlayer2Pos, Quaternion.identity);
+
     }
     void newMap()
     {
-         SceneManager.LoadScene(sceneName: "level1");
+        SceneManager.LoadScene(sceneName: "level1");
     }
 }
