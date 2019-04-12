d624f266ca545b031488cc3b7259d2493d299613
Paul <pasimko13@gmail.com>
2019-04-12 10:48:31 -0700

    Players are now objects :D

--- stick-fight-unity/Assets/Scenes/scripts/GameController.cs
+++ stick-fight-unity/Assets/Scenes/scripts/GameController.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
     public Transform stickPlayer;
     public List<Transform> stickPlayerList = new List<Transform>();
+    public PlayerController player1;
+    public PlayerController player2;
 
     void Awake()
     {
-        for (int i = 0; i < 2; i++)
-        {
-            GameObject tempPlayerObj = new GameObject();
-            Transform tempPlayerTransform = tempPlayerObj.transform;
-            Vector3 stickPlayerPos = new Vector3(Random.Range(-5, 5), 6, 10);
-            tempPlayerTransform = Instantiate(stickPlayer, stickPlayerPos, Quaternion.identity);
-            stickPlayerList.Add(tempPlayerTransform);
-        }
+        player1 = new PlayerController();
+        player2 = new PlayerController();
+
+        player1.Initialize(stickPlayer, new Vector3(Random.Range(-16, -3), 6, 10));
+        player2.Initialize(stickPlayer, new Vector3(Random.Range(3, 16), 6, 10));
     }
     void newMap()
     {
-         SceneManager.LoadScene(sceneName: "level1");
+        SceneManager.LoadScene(sceneName: "level1");
     }
 }
