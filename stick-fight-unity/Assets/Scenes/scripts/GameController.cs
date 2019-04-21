ae432945ee2b5dfc4b2bcc057d8c16f2fc99c71b
anders <anderscpeterson@gmail.com>
2019-04-21 14:52:08 -0700

    HealthBars in GameController

--- stick-fight-unity/Assets/Scenes/scripts/GameController.cs
+++ stick-fight-unity/Assets/Scenes/scripts/GameController.cs
@@ -2,21 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public Transform stickPlayer;
     public List<Transform> stickPlayerList = new List<Transform>();
-    public PlayerController player1;
-    public PlayerController player2;
+    PlayerController player1;
+    PlayerController player2;
+    public Image p1HealthBar;
+    public Image p2HealthBar;
+
 
     void Awake()
     {
+
+      
+
         player1 = new PlayerController();
         player2 = new PlayerController();
 
         player1.Initialize(stickPlayer, new Vector3(Random.Range(-16, -3), 6, 10));
         player2.Initialize(stickPlayer, new Vector3(Random.Range(3, 16), 6, 10));
+
+
+
+
+        p1HealthBar.fillAmount = 0.5f;
+        p2HealthBar.fillAmount = 0.17f;
+
+
+
     }
     void newMap()
     {
