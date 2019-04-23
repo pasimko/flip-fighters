26afc1ed71fb579951553be0d501fc05522ac1ea
Paul <pasimko13@gmail.com>
2019-04-22 20:17:51 -0700

    Init projectiles

---
+++ stick-fight-unity/Assets/Scenes/scripts/Projectile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Transform projectile;
+    //Create a projectile with these initial properties
+    public void Initialize(Vector3 position, Vector3 velocity, Vector3 size, float lifetime, float damage, List<PlayerController> targets, Transform prefab)
+    {
+        projectile = Instantiate(prefab, position, Quaternion.identity);
+    }
+}
