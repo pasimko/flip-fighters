98c60cd8eab2d91676f116978255df4ed3c19dd6
Paul <pasimko13@gmail.com>
2019-04-24 11:32:24 -0700

    pROjectiles

--- stick-fight-unity/Assets/Scenes/scripts/Projectile.cs
+++ stick-fight-unity/Assets/Scenes/scripts/Projectile.cs
@@ -11,4 +11,14 @@ public class Projectile : MonoBehaviour
         projectile = Instantiate(prefab, position, Quaternion.identity);
         projectile.velocity = velocity;
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contact = collision.contacts[0];
+
+        // Rotate the object so that the y-axis faces along the normal of the surface
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point;
+        Destroy(gameObject);
+    }
 }
