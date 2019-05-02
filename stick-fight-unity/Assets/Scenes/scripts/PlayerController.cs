b36a7e833783cb6c45194a3adbd8654b31948336
Paul <pasimko13@gmail.com>
2019-05-01 20:57:30 -0700

    guns woo

--- stick-fight-unity/Assets/Scenes/scripts/PlayerController.cs
+++ stick-fight-unity/Assets/Scenes/scripts/PlayerController.cs
@@ -15,6 +15,7 @@ public class PlayerController : MonoBehaviour
     public bool isJumping = false;
     public LayerMask groundLayer; // The map - Layer for checking collisions with any of the map
 
+    //Flip counting
     private float totalDegrees = 0;
     private Vector3 lastPoint;
     
@@ -22,12 +23,19 @@ public class PlayerController : MonoBehaviour
     public gunScript currentGun;
     public Transform meleePrefab;
 
+    //how many seconds we wait until raising the gun
+    float standCount = 0.5f;
+    //The rigidbody the gun is a child of
+    public Rigidbody2D gunHand;
+
     void Start()
     {
         lastPoint = transform.TransformDirection(Vector3.right);
         lastPoint.y = 0;
         // Body can get stuck in itself without this
         Physics2D.IgnoreCollision(leftLeg.GetComponent<BoxCollider2D>(), leftArm.GetComponent<BoxCollider2D>());
+        Physics2D.IgnoreCollision(head.GetComponent<BoxCollider2D>(), leftArm.GetComponent<BoxCollider2D>());
+        Physics2D.IgnoreCollision(head.GetComponent<BoxCollider2D>(), rightArm.GetComponent<BoxCollider2D>());
         Physics2D.IgnoreCollision(rightLeg.GetComponent<BoxCollider2D>(), rightArm.GetComponent<BoxCollider2D>());
     }
 
@@ -38,6 +46,7 @@ public class PlayerController : MonoBehaviour
         isGrounded = (Physics2D.OverlapCircle(leftToe.position, 0.2f, groundLayer) || Physics2D.OverlapCircle(rightToe.position, 0.2f, groundLayer));
         standUp();
         HandleMovement();
+        raiseArm();
         if (!isGrounded)
         {
             CountFlips();
@@ -97,15 +106,32 @@ public class PlayerController : MonoBehaviour
         lastPoint = facing;
 
     }
-    public void standUp()
+    void standUp()
     {
         if (isGrounded)
         {
+            standCount -= Time.deltaTime;
             isJumping = false;
             head.AddForce(new Vector2(0, 90));
             rightLeg.AddForce(new Vector2(0, -40));
             leftLeg.AddForce(new Vector2(0, -40));
             body.AddTorque(-body.angularVelocity);
+        }
+        else {
+            standCount = 0.5f;
+        }
+    }
+    void raiseArm() 
+    {
+        if (isGrounded && hasGun && standCount <= 0) {
+            float currentAngle = gunHand.transform.localEulerAngles.z;
+            if (currentAngle < 120) {
+                gunHand.AddTorque((90-currentAngle));
+                gunHand.AddTorque(-gunHand.angularVelocity/10);
+            }
+        }
+        else {
+
         }
     }
 }
--- stick-fight-unity/Assets/Scenes/scripts/gunScript.cs
+++ stick-fight-unity/Assets/Scenes/scripts/gunScript.cs
@@ -27,30 +27,26 @@ public class gunScript : MonoBehaviour
     {
         tempBullet = Instantiate(projectilePrefab, barrel.position, transform.rotation);
         tempBullet.gameObject.GetComponent<Projectile>().owner = equippedBy;
-        Physics2D.IgnoreCollision(tempBullet.GetComponent<Collider2D>(), equippedBy.GetComponent<PlayerController>().head.GetComponent<Collider2D>());
-        Physics2D.IgnoreCollision(tempBullet.GetComponent<Collider2D>(), equippedBy.GetComponent<PlayerController>().body.GetComponent<Collider2D>());
-        Physics2D.IgnoreCollision(tempBullet.GetComponent<Collider2D>(), equippedBy.GetComponent<PlayerController>().leftLeg.GetComponent<Collider2D>());
-        Physics2D.IgnoreCollision(tempBullet.GetComponent<Collider2D>(), equippedBy.GetComponent<PlayerController>().rightLeg.GetComponent<Collider2D>());
-        Physics2D.IgnoreCollision(tempBullet.GetComponent<Collider2D>(), equippedBy.GetComponent<PlayerController>().leftArm.GetComponent<Collider2D>());
-        Physics2D.IgnoreCollision(tempBullet.GetComponent<Collider2D>(), equippedBy.GetComponent<PlayerController>().rightArm.GetComponent<Collider2D>());
+        ignorePlayer(tempBullet);
         Physics2D.IgnoreCollision(tempBullet.GetComponent<Collider2D>(), gameObject.GetComponent<Collider2D>());
-
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
         //player1
         if (!equipped) {
             if (collision.collider.gameObject.layer == 10 && !player1.GetComponent<PlayerController>().hasGun) {
+                equippedBy = player1;
                 Transform playerHand = GameObject.Find("player1/rightArm/gunHolder").transform;
                 equipGun(ref playerHand, ref player1);
-                gameObject.transform.localScale = new Vector3(-0.3f/0.1592316f, 0.3f, 0.3f);
-                equippedBy = player1;
+                gameObject.transform.localScale = new Vector3(-1f, -1f, 1f);
+                ignorePlayer(gameObject);
             }
             else if (collision.collider.gameObject.layer == 11 && !player2.GetComponent<PlayerController>().hasGun) {
+                equippedBy = player2;
                 Transform playerHand = GameObject.Find("player2/leftArm/gunHolder").transform;
                 equipGun(ref playerHand, ref player2);
-                gameObject.transform.localScale = new Vector3(-0.3f/0.1592316f, -0.3f, 0.3f);
-                equippedBy = player2;
+                gameObject.transform.localScale = new Vector3(-1f, -1f, 1f);
+                ignorePlayer(gameObject);
             }
         }
     }
@@ -62,9 +58,22 @@ public class gunScript : MonoBehaviour
         player.GetComponent<PlayerController>().hasGun = true;
         player.GetComponent<PlayerController>().currentGun = gameObject.GetComponent<gunScript>();
         Destroy(gameObject.GetComponent<Rigidbody2D>());
-        //Destroy(gameObject.GetComponent<BoxCollider2D>());
-        gameObject.transform.SetParent(playerHand, false);
+        Destroy(gameObject.GetComponent<BoxCollider2D>());
+        gameObject.transform.SetParent(playerHand, true);
         gameObject.transform.localPosition = Vector3.zero;
         gameObject.transform.localRotation = Quaternion.identity;
+        gameObject.transform.localRotation = Quaternion.AngleAxis(90, Vector3.forward);
+        //gameObject.transform.localEulerAngles.y=0;
+        //gameObject.transform.localEulerAngles.z=0;
     }
+
+    void ignorePlayer(GameObject ignored) {
+        Physics2D.IgnoreCollision(ignored.GetComponent<Collider2D>(), equippedBy.GetComponent<PlayerController>().head.GetComponent<Collider2D>());
+        Physics2D.IgnoreCollision(ignored.GetComponent<Collider2D>(), equippedBy.GetComponent<PlayerController>().body.GetComponent<Collider2D>());
+        Physics2D.IgnoreCollision(ignored.GetComponent<Collider2D>(), equippedBy.GetComponent<PlayerController>().leftLeg.GetComponent<Collider2D>());
+        Physics2D.IgnoreCollision(ignored.GetComponent<Collider2D>(), equippedBy.GetComponent<PlayerController>().rightLeg.GetComponent<Collider2D>());
+        Physics2D.IgnoreCollision(ignored.GetComponent<Collider2D>(), equippedBy.GetComponent<PlayerController>().leftArm.GetComponent<Collider2D>());
+        Physics2D.IgnoreCollision(ignored.GetComponent<Collider2D>(), equippedBy.GetComponent<PlayerController>().rightArm.GetComponent<Collider2D>());
+    }
+
 }
