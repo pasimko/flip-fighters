12e78a3803634971b221a0eeb0b9a7fc3fc04b04
Ike <ike.lear@gmail.com>
2019-05-09 12:45:14 -0700

    Colors Work

---
+++ stick-fight-unity/Assets/Scenes/scripts/GlobalController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalController : MonoBehaviour
+{
+    public static GlobalController Instance;
+
+    public Color player1Color;
+    public Color player2Color;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
