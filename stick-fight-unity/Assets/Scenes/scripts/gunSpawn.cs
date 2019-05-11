e5a38b7e442fe4e4673b2e171f91e9bf1a92493d
Paul <pasimko13@gmail.com>
2019-05-10 17:45:43 -0700

    fixed gunspawn

--- stick-fight-unity/Assets/Scenes/scripts/gunSpawn.cs
+++ stick-fight-unity/Assets/Scenes/scripts/gunSpawn.cs
@@ -7,14 +7,17 @@ public class gunSpawn : MonoBehaviour
     private float nextActionTime = 0.0f;
     public float period = 0.1f;
     public GameObject auto;
+    float fakeTime = Time.time;
 
     void Start()
     {
-
+        fakeTime = Time.time;
+        nextActionTime = 0f;
+        period = 15f;
     }
     void Update()
     {
-        if (Time.time > nextActionTime)
+        if (Time.time - fakeTime > nextActionTime)
         {
             nextActionTime += period;
             Instantiate(auto, transform.position, transform.rotation);
